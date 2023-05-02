e68f029dd7b7c6d550f4e3d4ae3bc4d9cd17348c
unknown <anikm2982@gmail.com>
2023-05-02 16:55:54 +0600

    firebase setup done

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDFy6AgrWzutyxu0j6Mkxc9_lhu1rIOelM",
+  authDomain: "chef-s-table-84d17.firebaseapp.com",
+  projectId: "chef-s-table-84d17",
+  storageBucket: "chef-s-table-84d17.appspot.com",
+  messagingSenderId: "82162886495",
+  appId: "1:82162886495:web:741d2f0ba85efd104878ea"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
