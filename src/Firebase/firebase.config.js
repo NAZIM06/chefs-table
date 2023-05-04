7458a47d3761eceb0e2c7a8692da33ca56a37367
unknown <anikm2982@gmail.com>
2023-05-04 16:07:53 +0600

    env created

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDFy6AgrWzutyxu0j6Mkxc9_lhu1rIOelM",
-  authDomain: "chef-s-table-84d17.firebaseapp.com",
-  projectId: "chef-s-table-84d17",
-  storageBucket: "chef-s-table-84d17.appspot.com",
-  messagingSenderId: "82162886495",
-  appId: "1:82162886495:web:741d2f0ba85efd104878ea"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
